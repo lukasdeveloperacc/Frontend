c44fb4f1542e1ea3cd56baf090c1a9662c59f879
reconlabs-luke <luke@reconlabs.ai>
2025-05-19 21:21:10 +0900

    undo

--- DocumentManagementSystem/vite.config.ts
+++ DocumentManagementSystem/vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-const env = loadEnv("dev", ".");
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: env.BASE_URL,
+  base: "/DocumentManagementSystem/",
   // server: {
   //   host: "0.0.0.0",
   //   port: 5173,
